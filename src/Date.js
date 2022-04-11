14329571a3decddb400b30132a5b147c79a9a1cd
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-11 19:40:00 -0400

    Restructured files

---
+++ src/Date.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Date.css";
+
+export default function Date(){
+    return(
+        <h3 className="Date">Thursday 8:00</h3>
+    );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,43 @@
 import React from "react";
+import Date from "./Date";
 import "./styles.css";
-import Form from "./Form";
-import Main from "./Main";
+
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <Form />
-      <h3 className="Date">Thursday 8:00</h3>
-      <Main />
-    </div>
+     <form>
+      <input type="search" className="SearchBox" />{" "}
+      <input type="submit" value="Search" className="Button" />
+    </form>
+      <Date />
+     <div className="row">
+     <div className="col-6">
+       <h1>Verona, NJ</h1>
+       <h2>
+         <span className="MainTemp">33</span>°F
+       </h2>
+       <h3 id="Conditions">Cloudy</h3>
+       <div>
+         <img
+           src="http://openweathermap.org/img/wn/10d@2x.png"
+           width="100px"
+           className="CurrentEmoji" alt="weather emoji"
+         />
+       </div>
+     </div>
+     <div className="col-6">
+       <ul class="Factors">
+         <li>
+           Humidity: <span className="Humid">30</span>%
+         </li>
+         <li>
+           Wind: <span className="Wind">5</span> mph
+         </li>
+       </ul>
+     </div>
+   </div> 
+   </div>
   );
 }
 
