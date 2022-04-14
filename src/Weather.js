c2c2b8ddfd5a6c98d9a0d0b73e3a7dbc455074fa
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-13 21:04:41 -0400

    Updated api

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Date from "./Date";
 import axios from "axios";
 import "./styles.css";
 
 
 export default function Weather() {
+  const [ready, setReady]= useState(false);
+  const [weatherData, setWeatherData]= useState({});
+
   function handleResponse(response){
     console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon
+ });
+    setReady(true);
   }
-  let apiKey="d5051b82a85f7e540a240206a4a2fed4";
-  let city="London";
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
+  if (ready){
+      return (
     <div className="Weather">
      <form>
       <input type="search" className="SearchBox" />{" "}
@@ -21,14 +30,14 @@ export default function Weather() {
       <Date />
      <div className="row">
      <div className="col-6">
-       <h1>Verona, NJ</h1>
+       <h1>{weatherData.city}</h1>
        <h2>
-         <span className="MainTemp">33</span>°F
+         <span className="MainTemp">{Math.round(weatherData.temperature)}</span>°C
        </h2>
-       <h3 id="Conditions">Cloudy</h3>
+       <h3 id="Conditions" className="text-capitalize">{weatherData.description}</h3>
        <div>
          <img
-           src="http://openweathermap.org/img/wn/10d@2x.png"
+            src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
            width="100px"
            className="CurrentEmoji" alt="weather emoji"
          />
@@ -37,15 +46,21 @@ export default function Weather() {
      <div className="col-6">
        <ul class="Factors">
          <li>
-           Humidity: <span className="Humid">30</span>%
+           Humidity: <span className="Humid">{Math.round(weatherData.humidity)}</span>%
          </li>
          <li>
-           Wind: <span className="Wind">5</span> mph
+           Wind: <span className="Wind">{Math.round(weatherData.wind)}</span> mph
          </li>
        </ul>
      </div>
    </div> 
    </div>
   );
+  } else{ let apiKey="d5051b82a85f7e540a240206a4a2fed4";
+  let city="New York";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+return "Loading";
+}
 }
 
