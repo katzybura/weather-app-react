4dfce25cd9e7009cd068150c5bf2af46e15976c1
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-15 10:44:51 -0400

    Created WeatherForecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./styles.css";
 
@@ -43,7 +44,7 @@ setCity(event.target.value);
       <input type="submit" value="Search" className="Button" />
     </form>
     <WeatherInfo data={weatherData}/>
-      
+    <WeatherForecast />  
    </div>
   );
   } else{ 
---
+++ src/WeatherForecast.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function WeatherForecast(){
+    return (
+<h2>Forecast</h2>
+    );
+}
