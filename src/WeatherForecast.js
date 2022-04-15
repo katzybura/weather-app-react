ae09fe0103678fc001616cfee40fcb935593d69a
katzybura <97856084+katzybura@users.noreply.github.com>
2022-04-15 10:56:05 -0400

    Created WeatherForecast css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast(){
     return (
-<h2>Forecast</h2>
+<div className="WeatherForecast">
+    <div className="row">
+        <div className="col">
+            Fri
+        </div>
+        </div>
+        </div>
     );
 }
